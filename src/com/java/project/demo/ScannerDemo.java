5494570a0a8d5e851e58320ebd668636630ddad0
zhangyan18 <zhangyan18@asiainfo.com>
2022-08-12 17:59:50 +0800

    111

--- src/com/java/project/demo/ScannerDemo.java
+++ src/com/java/project/demo/ScannerDemo.java
@@ -12,8 +12,25 @@ public class ScannerDemo {
 
     //主入口
     public static  void main (String[] args){
-
+        //键盘录入测试一
         scannerTest(args);
+        //键盘录入测试二
+        scannerTest1(args);
+    }
+
+    private static void scannerTest1(String[] args) {
+        System.out.println("--进入scanner程序--开始--");
+        //2、创建对象，
+        Scanner sc = new Scanner(System.in);
+
+        //3、接收第一个数据
+        System.out.println("--请输入第一个数--");
+        int num1=sc.nextInt();
+        //3、接收第二个数据
+        System.out.println("--请输入第二个数--");
+        int num2=sc.nextInt();
+        System.out.println("计算的结果是:"+num1+num2);
+        System.out.println("--退出scanner程序--结束--");
     }
 
     private static void scannerTest(String[] args) {
